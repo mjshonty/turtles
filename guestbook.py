97e3969b09a382b328804e924aec157e7d869856
Mike Shonty <158367706+mjshonty@users.noreply.github.com>
2024-02-13 13:08:04 -0600

    initial commit

---
+++ guestbook.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+name = "Rick Wash"
+
+write(name)
+
+penup()
+right(90)
+forward(50)
+left(90)
+pendown()
+
+done()
---
+++ person.py
@@ -0,0 +1,30 @@
+from turtle import *
+
+right(90)
+
+# Draw a line
+forward(75)
+
+# Go back to the center
+back(75)
+
+# Draw a circle for a head
+left(90)
+circle(25)
+right(90)
+
+# Move down the line
+forward(25)
+
+# Draw right arm
+
+# Draw left arm
+
+# Move down to bottom of line
+
+# Left Leg
+
+# Right leg
+
+# Wait for the user to close the window
+done()
