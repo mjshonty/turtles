820fb8e657a1937ecd93e7a929190b3d2eda474d
Mike Shonty <mjshonty@gmail.com>
2024-02-13 20:04:08 -0600

    Hid turtle while setting inital position

--- stars.py
+++ stars.py
@@ -1,12 +1,16 @@
 from turtle import *
 
+hideturtle()
 penup()
 setpos(-300,0)
 
 for i in range(0,5):
+    
     penup()
     forward(100)
     pendown()
+    showturtle()
+    
     for j in range (0,5):
         right(144)
         forward(50)
