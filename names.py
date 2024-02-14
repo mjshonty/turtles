2740cb28dbcd0b2e67a097ea383e1596bf81c474
Mike Shonty <mjshonty@gmail.com>
2024-02-13 18:44:37 -0600

    finished names.py

---
+++ names.py
@@ -0,0 +1,46 @@
+from turtle import *
+
+write("Kevin Werth")
+
+pencolor("red")
+back(10)
+forward(90)
+
+left(90)
+forward(20)
+
+left(90)
+forward(100)
+
+left(90)
+forward(20)
+
+left(90)
+forward(15)
+
+penup()
+
+forward(110)
+
+pencolor("black")
+write("Mike Shonty")
+
+pendown()
+pencolor("red")
+
+back(10)
+forward(90)
+
+left(90)
+forward(20)
+
+left(90)
+forward(100)
+
+left(90)
+forward(20)
+
+left(90)
+forward(15)
+
+done()
