849d9962cd858cff53a8eed8c9a923ea6db7a374
Mike Shonty <mjshonty@gmail.com>
2024-02-13 18:26:09 -0600

    Completed several exercises

--- person.py
+++ person.py
@@ -17,14 +17,28 @@ right(90)
 forward(25)
 
 # Draw right arm
+left(125)
+forward(75)
+back(75)
 
 # Draw left arm
+right(250)
+forward(75)
+back(75)
 
 # Move down to bottom of line
+left(125)
+forward(50)
 
 # Left Leg
+right(25)
+forward(75)
+back(75)
 
 # Right leg
+left(50)
+forward(75)
+back(75)
 
 # Wait for the user to close the window
 done()
--- square.py
+++ square.py
@@ -5,6 +5,8 @@ right(90)
 forward(100)
 right(90)
 forward(100)
+right(90)
+forward(100)
 
 
 done()
