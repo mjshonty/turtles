bb672eddf044e4bf2d056fc82da62d80a2e8559f
Mike Shonty <mjshonty@gmail.com>
2024-02-14 16:25:20 -0600

    used turtles.teleport to move turtle's position

--- stars.py
+++ stars.py
@@ -2,15 +2,15 @@ from turtle import *
 
 bgcolor("gray") #sets background color
 
-hideturtle() #hide the turtle while we move into the inital postion
-penup() #don't draw during the move to the inital position
-setpos(-300,0) #set starting position so we don't draw off the screen
+teleport(-300,0) #set turtle's position
+
+#Uncomment speed(0) to make turtle draw quickly - makes testing faster
+#speed(0) 
 
 color("yellow", "green") #set pen color yellow and fill color green
 begin_fill() #fill all shapes drawn in the following code
 
 for i in range(0,5): #loop starting each star 100 pixels away from it's origina point
-    
     penup()
     forward(100)
     pendown()
@@ -24,3 +24,15 @@ end_fill() #Everything drawn between being and end fill should be filled with th
            #Note that according to turtle the middle of the shape may not fill, which is the case for it's behavior on my machine.
 
 done()
+
+
+
+for i in range(0,5): #loop starting each star 100 pixels away from it's origina point
+    penup()
+    forward(100)
+    pendown()
+    showturtle()
+    
+    for j in range (0,5): #loop turning right 144 degrees and moving forward 50 pixels 5 times to draw the star
+        right(144)
+        forward(50)
